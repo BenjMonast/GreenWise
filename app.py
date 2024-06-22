72ead1e40a320d546b42ad9e76cff2837aaf4513
Benjamin Monastyrsky <benmony1021@gmail.com>
2024-06-22 14:23:58 -0700

    made multiple pages

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/carbon")
+def carbon():
+    return render_template("carbon.html")
+
+@app.route("/manual")
+def manual():
+    return render_template("manual.html")
