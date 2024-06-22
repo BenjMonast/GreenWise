2162c895fc3dd73a9ec5f60f53a352614fef8d78
inkyant <80181074+inkyant@users.noreply.github.com>
2024-06-22 15:04:24 -0700

    add database, embeddings

---
+++ database.py
@@ -0,0 +1,457 @@
+import numpy
+db = numpy.array([[
+    "VANILLA, CHOCOLATE HAZELNUT, PISTACHIO, SALTED CARAMEL, RASPBERRY AND LEMON MACARONS ASSORTMENT, VANILLA, CHOCOLATE HAZELNUT, PISTACHIO, SALTED CARAMEL, RASPBERRY AND LEMON",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.07
+],
+[
+    "MIXED BERRY STRAWBERRY, CHERRY, GRAPE & RASPBERRY FRUIT-FLAVORED SNACKS, MIXED BERRY",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.84
+], 
+[
+    "ORANGE FUDGE DIPPED PRETZEL RODS WITH WHITE FUDGE DRIZZLE, ORANGE FUDGE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.19
+], 
+[
+    "FROSTED CHOCOLATE SUGAR COOKIES, FROSTED CHOCOLATE SUGAR",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.61
+], 
+[
+    "CHOCOLATE TWISTED TAVERN COOKIE KIT, CHOCOLATE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.61
+], 
+[
+    "MOZZARELLA LOW-MOISTURE PART-SKIM FINE CUT CHEESE, MOZZARELLA",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    18.11
+], 
+[
+    "MARKET PANTRY, WHIPPED CREAM CHEESE SPREAD, CHIVE, CHIVE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    18.11
+], 
+[
+    "YARD OF MARSHMALLOWS HOT COCOA TOPPERS, YARD OF MARSHMALLOWS",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.84
+], 
+[
+    "MULTIGRAIN OATMEAL, TRIPLE BERRY",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.61
+], 
+[
+    "FROSTED SUGAR COOKIES, FROSTED SUGAR",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.61
+], 
+[
+    "BUTTERMILK PANCAKE & WAFFLE MIX, BUTTERMILK",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.15
+], 
+[
+    "SIMPLY BALANCED, ORGANIC WHITE CORN TORTILLA CHIPS",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    3.99
+], 
+[
+    "HAUNTED MANSION CHOCOLATE COOKIE KIT, CHOCOLATE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.15
+], 
+[
+    "CHOCOLATE ICE CREAM, CHOCOLATE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.52
+], 
+[
+    "WITCHES' BREW SLIME PUNCH LIME EDIBLE SLIME DRINK MIX, WITCHES' BREW",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    10.81
+], 
+[
+    "HEARTY WHITE HOT DOG BUNS",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.41
+], 
+[
+    "CHOCOLATE MINI CUPCAKES, CHOCOLATE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.85
+], 
+[
+    "RED RASPBERRIES",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    3.15
+], 
+[
+    "DARK CHOCOLATE, COCAO",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    9.61
+], 
+[
+    "CHOCOLATE COOKIE",
+    "Target Stores",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.61
+],
+[
+    "Beef meatballs, raw",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    21.20,
+],
+[
+    "Minced soya, frozen",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    2.64,
+],
+[
+    "Minced broad bean protein, frozen",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    2.13,
+],
+[
+    "Soft cheese from cow's milk",
+    "CarbonCloud",
+    "Food product",
+    "Italy",
+    "At store",
+    12.86,
+],
+[
+    "Beef, bottom round roast",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    33.12,
+],
+[
+    "Chicken kebab",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    3.59,
+],
+[
+    "Pork, spare ribs",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    7.03,
+],
+[
+    "Beef, ribeye roast, boneless",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    46.19,
+],
+[
+    "Minced pea protein, frozen",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Sweden",
+    "At store",
+    2.35,
+],
+[
+    "Served Raspberry Hard Seltzer",
+    "Served Drinks",
+    "Food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    0.86,
+],
+[
+    "Spreadable 75%% fat (butter + veg. oils)",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Denmark",
+    "At store",
+    7.27,
+],
+[
+    "Root Veg Nugget - UK",
+    "Strong Roots",
+    "Food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    1.83,
+],
+[
+    "Red wine, 0.75L glass bottle",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "Netherlands",
+    "At store",
+    1.49,
+],
+[
+    "Oatly Oat Drink Barista Edition 1 L (Chilled)",
+    "oatly",
+    "Food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    0.50,
+],
+[
+    "Meat soup in glass jar",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    2.68,
+],
+[
+    "Protein shake, dairy based",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    2.27,
+],
+[
+    "Carp, edible weight, farmed",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    7.81,
+],
+[
+    "Sweet Potato Hash Browns - UK",
+    "Strong Roots",
+    "Food product",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "At store",
+    1.07,
+],
+[
+    "Carbonated soft drink, aluminium can",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "United States of America",
+    "At store",
+    1.35
+],
+[
+    "Oatly Strawberry 1 pint (Plant-based Frozen Dessert)",
+    "oatly",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.35
+],
+[
+    "Mediterranean Orzo Bake - USA",
+    "Strong Roots",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.37
+],
+[
+    "pineapple chunks in juice, 20 oz. tin can",
+    "Dole Sunshine Company",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.86
+],
+[
+    "pineapple crushed in syrup, 20 oz. tin can",
+    "Dole Sunshine Company",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.75
+],
+[
+    "pineapple crushed in juice, 20 oz. tin can",
+    "Dole Sunshine Company",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.87
+],
+[
+    "Thai Green Veg Curry - USA",
+    "Strong Roots",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.75
+],
+[
+    "Mexican orange juice, freshly squeezed, 1L",
+    "CarbonCloud Benchmark",
+    "Benchmark food product",
+    "United States of America",
+    "At store",
+    1.37
+],
+[
+    "Chipotle Mac - USA",
+    "Strong Roots",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.36
+],
+[
+    "Black dahl - USA",
+    "Strong Roots",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.49
+],
+[
+    "7 SELECT, GOURMET JELLY BEANS",
+    "7-Eleven, Inc.",
+    "Food product",
+    "United States of America",
+    "At store",
+    1.84
+],
+[
+    "HABANERO+LIME ROLLED TORTILLA CHIPS, HABANERO+LIME",
+    "7-Eleven, Inc.",
+    "Food product",
+    "United States of America",
+    "At store",
+    3.99
+],
+[
+    "CRUNCH BAR, STRAWBERRY",
+    "7-Eleven, Inc.",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.52
+],
+[
+    "NUTRITION BARS, COOKIES & CREAM COOKIE DOUGH",
+    "Abbott Laboratories Inc",
+    "Food product",
+    "United States of America",
+    "At store",
+    3.33
+],
+[
+    "WATERMELON FLAVORED SODA, WATERMELON",
+    "7-Eleven, Inc.",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.06
+],
+[
+    "STRAWBERRY SHORTCAKE BAR, STRAWBERRY",
+    "Abbott Laboratories Inc",
+    "Food product",
+    "United States of America",
+    "At store",
+    3.33
+],
+[
+    "APPLE PIE SANDWICH COOKIES, APPLE PIE",
+    "7-Eleven, Inc.",
+    "Food product",
+    "United States of America",
+    "At store",
+    4.61
+],
+[
+    "ORIGINAL NUTRITION SHAKE, VANILLA",
+    "Abbott Laboratories Inc",
+    "Food product",
+    "United States of America",
+    "At store",
+    2.26,
+],
+[
+    "PEANUT BUTTER PIE KETO BARS, PEANUT BUTTER PIE",
+    "Abbott Laboratories Inc",
+    "Food product",
+    "United States of America",	
+    "At store",
+    3.33
+]])
