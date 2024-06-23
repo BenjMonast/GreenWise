073925687f1344d936608307d58bec8462a04c54
Benjamin Monastyrsky <benmony1021@gmail.com>
2024-06-22 21:00:54 -0700

    Finished taggun and redcircle integration

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
-from tokens import OAI_TOKEN
-import base64, requests, re
+from tokens import *
+import base64, requests, re, pickle
 
 app = Flask(__name__)
 
@@ -21,40 +21,56 @@ def read_receipt():
     file = request.files['file']
     base64_image = base64.b64encode(file.read()).decode('utf-8')
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {OAI_TOKEN}"
-    }
+    csv_text = None
+
+    url = "https://api.taggun.io/api/receipt/v1/verbose/encoded"
 
     payload = {
-        "model": "gpt-4o",
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Compile the information on this receipt into a csv file with the columns Product, Price, and Category, where the possible Categories are Food, Household Essentials, Health and Beauty, Electronics, Clothing, Home and Furniture, Toys and Games, Office Supplies, Outdoor, Automotive, Baby, and Pet."
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": f"data:image/jpeg;base64,{base64_image}"
-                        }
-                    }
-                ]
-            }
-        ],
-        "max_tokens": 1000
+        "image": base64_image,
+        "filename": "image.jpg",
+        "contentType": "image/jpeg"
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "apikey": TAGGUN_TOKEN
     }
 
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    message = response.json()['choices'][0]['message']['content']
+    response = requests.post(url, json=payload, headers=headers)
 
+    items = []
+
+    for row in response.json()['amounts']:
+        text = row['text']
+        words = text.split(" ")
+        if not (words[0].isnumeric() and len(words[0]) == 9):
+            continue
+
+        items.append((words[0], words[-1]))
+
+    with open('data.pkl', 'rb') as f:
+        cache = pickle.load(f)
     
+    out = []
 
-    return {"content": message}
-    # return {}
+    for row in items:
+        id = row[0]
+        if id not in cache:
+            json = requests.get(f"https://api.redcircleapi.com/request?api_key={RC_TOKEN}&type=search&search_term={id}").json()
+            if "search_results" in json:
+                cache[id] = json["search_results"]
+            else:
+                cache[id] = None
+
+        if cache[id] == None:
+            continue
+
+        out.append([cache[id][0]['product']['title'], row[1]])
+
+    with open('data.pkl', 'wb') as f:
+        pickle.dump(cache, f)
+
+    return {"content": out}
 
 if __name__ == '__main__':
     app.run()
